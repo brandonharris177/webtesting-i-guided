92445b5c7040000ebbe30a26fb4af7f9813989fc
Brandon-Harris <brandonharris177@gmail.com>
2020-03-25 15:03:44 -0600

    all tests passing

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,26 @@ module.exports = {
   add,
 };
 
+function isNumber(array) {
+  const allNumbers = array.filter(value => typeof(value) === "number" && isNaN(value) === false);
+  if (array.length === allNumbers.length) {
+    return true
+  } else {
+    return false
+  }
+}
+
 function add() {
-  return null;
+  let array = Array.from(arguments)
+  if (array.length === 0) {
+    return 0
+  } else if (
+    isNumber(array)
+  ) { 
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let sum = array.reduce(reducer);
+    return sum;
+  } else {
+    throw new Error("Both arguments must be numbers")
+  }
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,44 @@
 const { add } = require('./calculator.js');
 
 // test away!
+it("should run tests", function() {
+    expect(true).toBe(true);
+});
+
+describe('calculator', function() {
+    describe('add', function () {
+        it('should return the sum of 2 numbers as arguments', function() {
+            // Arrange
+           
+            // Act
+                const sum = add(2, 2);
+            // Assert
+                expect(sum).toBe(4);
+        })
+
+        it('should return the sum of 2 numbers', function() {
+            expect(add(2, 2)).toBe(4);
+            expect(add(0, 2)).toBe(2);
+            expect(add(-2, 2)).toBe(0);
+            expect(add(1, 10)).toBe(11);
+        })
+
+        it('should only take numbers', function() {
+            expect(() =>{
+                add(2, "two")
+            }).toThrow();
+
+            expect(() =>{
+                add(2, NaN)
+            }).toThrow();
+
+            expect(() =>{
+                add(2, {})
+            }).toThrow();
+
+            expect(() =>{
+                add(2, [])
+            }).toThrow();
+        })
+    })
+})
